2a23f542c90fdbaf3f0098dea116da203740a019
yoanarobeva <yoana_robeva@yahoo.com>
2023-04-03 21:35:42 +0300

    some not used components -> deleted

--- client/src/components/_NOT_USED_DELETE/Spinner/Spinner.js
+++
@@ -1,7 +0,0 @@
-export const Spinner = () => {
-    return (
-        <div className="spinner-grow" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
-    );
-};
