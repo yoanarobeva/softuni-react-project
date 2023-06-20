c86a705fb6bdd4ffc62e374c6bde0c7174c32f45
yoanarobeva <yoana_robeva@yahoo.com>
2023-06-20 16:29:06 +0300

    change

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
    
     <BrowserRouter>
 
@@ -16,7 +16,7 @@ root.render(
       
     </BrowserRouter>
     
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 reportWebVitals();
